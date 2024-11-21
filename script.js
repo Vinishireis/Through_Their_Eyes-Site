374fc4e0a8facc010f16e29a0ddb69a0c4f66888
Vinishireis <nishimuravinicius28@gmail.com>
2024-11-20 22:30:46 -0300

    Atualização e modificação no Site

--- script.js
+++ script.js
@@ -41,3 +41,26 @@ backToTop.addEventListener("click", function (e) {
   e.preventDefault();
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+
+// Carrossel Fotos//
+const carouselItems = document.querySelectorAll(".carousel_item"); 
+let i = 1;
+
+setInterval(() => {
+// Accessing All the carousel Items
+ Array.from(carouselItems).forEach((item,index) => {
+
+   if(i < carouselItems.length){
+    item.style.transform = `translateX(-${i*100}%)`
+   }
+  })
+
+
+  if(i < carouselItems.length){
+    i++;
+  }
+  else{
+    i=0;
+  }
+},2000)
